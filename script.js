16a1c6f1bfc6d38580b7247e60b01608d6d1ebee
B-lovedth <greatsoma2019@gmail.com>
2022-10-18 17:03:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,53 @@
-const cellElements = document.querySelectorAll('[data-cell]')
-const board = document.getElementById('board')
-const x_class = 'x'
-const o_class = "o"
-let o_turn = false;
+const cellElements = document.querySelectorAll("[data-cell]");
+const board = document.getElementById("board");
+const x_class = "x";
+const o_class = "o";
+const messageContainer = document.querySelector("#message-container");
+const message = document.querySelector("[message-text]");
+let o_turn;
 const winningCombination = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 const handleClick = (e) => {
-    const cell = e.target;
-    let currentClass = o_turn ? o_class : x_class;
-    placeMark(cell, currentClass);
-    o_turn = !o_turn;
-    setBoardHoverClass();    
-    if (checkWin(currentClass)) {
-        
-    }
+  const cell = e.target;
+  let currentClass = o_turn ? o_class : x_class;
+  placeMark(cell, currentClass);
+  o_turn = !o_turn;
+  setBoardHoverClass();
+  if (checkWin(currentClass)) {
+    endGame(false);
+  }
   console.log("clicked");
 };
-
-const placeMark = (cell,currentClass) => {
-    cell.classList.add(currentClass)
-}
-const setBoardHoverClass=()=>{
-    board.classList.remove(x_class)
-    board.classList.remove(o_class)
-    if (o_turn) {
-    board.classList.add(o_class);
-    } else {
-    board.classList.add(x_class);
-        
+const endGame = (draw) => {
+  if (draw) {
+  } else {
+    messageContainer.classList.add("show");
     }
-}
+    message.textContent = `${o_turn ? "X's" : "O's"} Win!`;
+    
+};
+
+const placeMark = (cell, currentClass) => {
+  cell.classList.add(currentClass);
+};
+const setBoardHoverClass = () => {
+  board.classList.remove(x_class);
+  board.classList.remove(o_class);
+  if (o_turn) {
+    board.classList.add(o_class);
+  } else {
+    board.classList.add(x_class);
+  }
+};
 const startGame = () => {
   cellElements.forEach((cell) => {
     cell.addEventListener("click", handleClick, { once: true });
@@ -47,9 +56,9 @@ const startGame = () => {
 };
 startGame();
 const checkWin = (currentClass) => {
-    return winningCombination.some(combination => {
-        return combination.every(index => {
-            return  cellElements[index]
-        })
-    })
-}
+  return winningCombination.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(currentClass);
+    });
+  });
+};
